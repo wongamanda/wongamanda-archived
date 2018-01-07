726ce7f3b7606006ddfac86f339451aa16525832
wongie <20161098@student.nths.net>
2018-01-07 01:39:54 -0600

    new pages

--- aw_js.js
+++ aw_js.js
@@ -1,92 +1,34 @@
-const littleMessage = document.getElementById("iconBBY");
+// var titleArray = ['a librarian', 'aspiring data scientist', 'lady-musician enthusiast'];
+// var titleArrayLength = titleArray.length;
+// var titleA = $('.epithet');
 
-function showElem(){
-    littleMessage.display = 'block';
-    hideElem();
-}
+// function rotate(i){
+//     var animationIn = 'animated fadeIn';
+//     var animationOut = 'animated fadeOut';
+//     var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
-function hideElem(){
-   littleMessage.display = 'none';
-}
+//     if (i == titleArrayLength){i=0;}
+    
+//     $('epithet').text(titleArray[i]).addClass(animationIn).one(animationEnd, function(){
+//         $(this).removeClass(animationIn);
+//     });
 
-var slideIndex = 1;
-showSlide(slideIndex);
+//     setTimeout(function() {
+//         rotate(i+1);
+//     })
+// }
 
-function changeSlide(n){
-    showSlide(slideIndex += n);
-}
+function add_remove(){
+    var animationIn = 'animated fadeIn';
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
-function currentSlide(n){
-    showSlide(slideIndex=n);
-}
-
-function showSlide(n){
-    var i;
-    var x = document.getElementsByClassName("slides");
-    if (n > x.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = x.length;
-    }
-    for (i = 0; i < x.length; i++){
-        x[i].style.display = "none";
-    }
-    x[slideIndex-1].style.display = "block";
-}
-
-function automaticSlide(){
-    var i;
-    var x = document.getElementsByClassName("slides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {
-        slideIndex = 1;
-    }
-    x[slideIndex-1].style.display = "block";
-    setTimeout(automaticSlide, 2000); // Change image every 2 seconds
-}
-
-
-function initMap(){
-//     var home = favoritePlaces[0];
-    var mapDiv = document.getElementById("map");
-    var map = new google.maps.Map(document.getElementById("map"), {
-        center: {lat: 40.753, lng: -73.997}, 
-        zoom: 13
+    $('#greeting').addClass(animationIn).one(animationEnd, function(){
+        $('#greeting').removeClass(animationIn);
         });
-    placeMarker(map);
-    }
+    };
 
-function placeMarker(map){
-    var favoritePlaces = [
-    ["NYC Home", 40.753, -73.997,1],
-    ["Whitney", 40.739551, -74.009063,1],
-    ["Dia: Chelsea", 40.748005, -74.006842],
-    ["Strand", 40.733104, -73.990726]
-    ];
-
-    var lengthPlace = favoritePlaces.length;
-    for (var i = 0; i < lengthPlace; i++){
-        var place = favoritePlaces[i];
-        var marker = new google.maps.Marker({
-            position: {lat: place[1], lng: place[2]},
-            map: map,
-            title: place[0],
-            zIndex: place[3]});
-            
-        var infowindow = new google.maps.InfoWindow({
-            content: place[0]
-            });
-        }
-        
-    }
-
-google.maps.event.addListener(marker, 'click', function(){
-            infowindow.open(map, marker)
-        });
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
+$(document).ready(function() {
+    add_remove();
+    $('#greeting').on('click', add_remove);
+    $(".flip_card").flip({trigger: 'hover'});
+});
